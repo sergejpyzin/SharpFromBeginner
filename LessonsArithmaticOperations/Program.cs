b81a335fecec3483ba3965102d2407270e6b5c57
Sergej Pyzin <sergejpyzin@yandex.ru>
2023-02-17 11:20:28 +0300

    Added LessonsArithmaticOperation

---
+++ LessonsArithmaticOperations/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LessonsArithmaticOperations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Запрашиваем у пользователя информацию
+            Console.WriteLine("Введите первое число: ");
+            int numberA = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите второе число: ");
+            int numberB = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите третье число: ");
+            int numberC = Convert.ToInt32(Console.ReadLine());
+
+            // Выполняем условия задачи
+            int resSum = numberA+ numberB + numberC;
+            int resMult = numberA * numberB * numberC;
+
+            // Выводим результат в консоль
+            Console.WriteLine($"Сумма введенных чисел равна - {resSum}; Произведение введенных чисел равно - {resMult}");
+            Console.ReadLine();
+        }
+    }
+}
