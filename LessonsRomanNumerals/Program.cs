f01863205b10edea072d36b7998f6fdc93c7bd84
Sergej Pyzin <sergejpyzin@yandex.ru>
2023-02-17 14:52:13 +0300

    Added Magick

---
+++ LessonsRomanNumerals/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LessonsRomanNumerals
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Запрашиваем информацию у пользователя
+            Console.WriteLine("Введите целое положительное число: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            // Обрабатываем информацию
+            do
+            {
+                int i = 0;
+                int localNumber;
+                number = number % 10;
+
+            } while (number % 10 != number);
+        }
+    }
+}
---
+++ Magick/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Magick
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            
+            int randomNumber = rnd.Next(1, 99);
+            Console.Write("Загадываем число.");
+            System.Threading.Thread.Sleep(800);
+            Console.Write(".");
+            System.Threading.Thread.Sleep(800);
+            Console.Write(".");
+            System.Threading.Thread.Sleep(800);
+            Console.WriteLine(" ");
+            Console.WriteLine($"Наше число: {randomNumber}!");
+            int firstRandomDigit = randomNumber / 10;
+            int secondRandomDigit = randomNumber % 10;
+            System.Threading.Thread.Sleep(2000);
+            Console.WriteLine("*** Производим магию вычислений! ***");
+            System.Threading.Thread.Sleep(2000);
+            Console.WriteLine($"Наша первая цифра: {firstRandomDigit}");
+            System.Threading.Thread.Sleep(300);
+            Console.WriteLine($"Наша вторая цифра: {secondRandomDigit}");
+            Console.Write("Считаем.");
+            System.Threading.Thread.Sleep(800);
+            Console.Write(".");
+            System.Threading.Thread.Sleep(800);
+            Console.Write(".");
+            Console.WriteLine(" ");
+            System.Threading.Thread.Sleep(800);
+            if (firstRandomDigit > secondRandomDigit) { Console.WriteLine($"Цифра {firstRandomDigit} больше чем {secondRandomDigit}!"); }
+            else if (firstRandomDigit < secondRandomDigit) { Console.WriteLine($"Цифра {secondRandomDigit} больше чем {firstRandomDigit}!"); }
+            else if (firstRandomDigit == secondRandomDigit) { Console.WriteLine($"Вот это повезло {firstRandomDigit} = {secondRandomDigit}"); }
+            else { Console.WriteLine("Магия не сработала :("); }
+        }
+    }
+}
